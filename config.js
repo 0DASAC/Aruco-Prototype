532ae23fc44d440df18d3173958ca7f3e2fe3a90
0DASAC <jos.montero@uc.cl>
2025-09-09 07:53:24 -0300

    setup basico

---
+++ config.js
@@ -0,0 +1,14 @@
+Protobject.setProduction(true);
+Protobject.initialize([
+  {
+    name: "Camera",
+    page: "camera.html",
+    debug: "local",
+  },
+  {
+    name: "Lamp",
+    page: "index.html",
+    main: true,
+    debug: "master",
+  },
+]);
